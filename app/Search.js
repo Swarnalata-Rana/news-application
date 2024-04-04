93863090b028cc3bf9f36ee78fcfe0a728bf80fd
Swarnalata <swarnalatarana21@navgurukul.org>
2024-04-04 13:55:12 +0530

    part3done

--- app/Search.js
+++ app/Search.js
@@ -1,18 +1,37 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 const Search = () => {
+    const [searchInput, setSearchInput] = useState('');
+
+
+    const handleSearch = () => {
+        fetch(`https://newsapi.org/v2/everything?q=${searchInput}&apiKey=4ec780570c234ab1a4bf02f7fec5516a`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)            
+
+            })
+            .catch(error => console.log(error));
+    };
+
     return (
         <>
-            <div class="row main-input-btn">
+            <div className="row main-input-btn">
                 <div className='input-btn'>
-                    <img src='search-logo.png' className='search-icon' />
-                    <input type="search" placeholder="Search" className='search-bar' />
-                    <button type="button" className='go-btn'>Go</button>
+                    <img src='search-logo.png' className='search-icon' alt="Search icon" />
+                    <input
+                        type="search"
+                        placeholder="Search"
+                        value={searchInput}
+                        className='search-bar'
+                        onChange={(event)=>setSearchInput(event.target.value)}
+                    />
+                    <button type="button" onClick={handleSearch} className='go-btn'>Go</button>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
+
 export default Search;
-
-
